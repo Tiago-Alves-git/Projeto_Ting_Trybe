08e10a8ced31a717e50518bfb8573fa98eb0e585
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2023-08-28 18:27:58 -0300

    :white_check_mark: adiciona metodo de importar arquivos txt
    requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
+import sys
+
+
+def txt_importer(path_file: str):
     """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write("Formato inválido")
+
+        with open(path_file, 'r') as file:
+            lines = file.read().split('\n')
+            return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
