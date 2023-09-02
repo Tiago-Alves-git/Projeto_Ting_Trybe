b31fdff7782e926cc947aed76d1bb0e1f331b864
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2023-09-02 14:17:52 -0300

    :white_check_mark: add process function
    req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
-def process(path_file, instance):
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
+
+
+def process(path_file, instance: Queue):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if path_file == instance.search(index)["nome_do_arquivo"]:
+            return None
+
+    linhas = txt_importer(path_file)
+
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(linhas),
+        "linhas_do_arquivo": linhas,
+    }
+
+    instance.enqueue(new_dict)
+
+    sys.stdout.write(str(new_dict))
 
 
 def remove(instance):
@@ -8,3 +28,10 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# {
+#     "nome_do_arquivo": "arquivo_teste.txt", Caminho do arquivo
+#     "qtd_linhas": 3, Quantidade de linhas existentes no arquivo
+#     "linhas_do_arquivo": [...] linhas retornadas pela função do requisito 2
+# }
